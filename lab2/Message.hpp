167d230c70039966376f98711f2f7c0fb9b3487a
Dmytro Tereshchenko <buxdmo@gmail.com>
2023-04-17 22:06:36 +0300

    Lab 2: Add Message class implementation

---
+++ lab2/Message.hpp
@@ -0,0 +1,61 @@
+#ifndef LAB2_MESSAGE_HPP
+#define LAB2_MESSAGE_HPP
+
+#include <chrono>
+
+namespace Lab2 {
+
+	enum class MessagePriority {
+		Low,
+		Medium,
+		High
+	};
+
+	using Clock		= std::chrono::steady_clock;
+	using TimePoint = std::chrono::time_point<Clock>;
+
+	template<class T>
+	class Message {
+	public:
+		using ValueType = T;
+		using Priority	= MessagePriority;
+
+	public:
+		Message(const ValueType& data, const TimePoint& expiryTime, Priority priority = Priority::Medium)
+			: m_priority{ priority }
+			, m_data{ data }
+			, m_expiryTime{ expiryTime }
+		{}
+
+		template <class Duration>
+		Message(const ValueType& data, const Duration& duration, Priority priority = Priority::Medium)
+			: m_priority{ priority }
+			, m_data{ data }
+			, m_expiryTime{ Clock::now() + duration }
+		{}
+
+	public:
+		bool isExpired() const noexcept {
+			return Clock::now() >= m_expiryTime;
+		}
+
+		Priority getPriority() const noexcept {
+			return m_priority;
+		}
+
+		ValueType getData() const noexcept {
+			return m_data;
+		}
+
+		TimePoint getExpiryTime() const noexcept {
+			return m_expiryTime;
+		}
+
+	private:
+		Priority m_priority;
+		ValueType m_data;
+		TimePoint m_expiryTime;
+	};
+}
+
+#endif // !LAB2_MESSAGE_HPP
