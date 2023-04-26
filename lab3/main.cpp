85c4af7b5cd34581b01fdeeeee452764413e61e5
Dmytro Tereshchenko <buxdmo@gmail.com>
2023-04-26 18:51:50 +0300

    Lab 3: Add explicit instantiation

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,4 +1,15 @@
-﻿int main()
+﻿#include "Matrix.hpp"
+#include "ConstMatrix.hpp"
+#include <iostream>
+
+using namespace Lab3;
+
+template class Matrix<std::string>;
+template class Matrix<int>;
+template class ConstMatrix<int, 4, 4>;
+
+
+int main()
 {
 	return 0;
 }
