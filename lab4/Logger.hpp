f21b5a38b98110d19928a19cfc824af2aad9d572
Dmytro Tereshchenko <buxdmo@gmail.com>
2023-04-29 01:07:47 +0300

    Lab 4: Add Logger file

---
+++ lab4/Logger.hpp
@@ -0,0 +1,39 @@
+#ifndef LAB4_LOGGER_HPP
+#define LAB4_LOGGER_HPP
+
+#include <fstream>
+#include <sstream>
+#include <thread>
+#include <mutex>
+#include "Util.hpp"
+
+namespace Lab4 {
+
+	class Logger {
+	public:
+		static Logger& instance() noexcept {
+			static Logger logger;
+			return logger;
+		}
+
+		template<typename ...Args>
+		void log(Args&& ...args) {
+			std::stringstream ss;
+			ss << '[' << currentDateTime() << "]\t";
+			ss << '[' << std::this_thread::get_id() << "]\t";
+			(ss << ... << args);
+			
+			std::lock_guard lock{ m_mutex };
+			m_file << ss.str() << '\n';
+		}
+
+	private:
+		std::ofstream m_file{ "log.txt", std::ios_base::app };
+		std::mutex m_mutex{};
+	};
+}
+
+#define LOG(...) \
+    Lab4::Logger::instance().log('[', __FUNCTION__, ':', __LINE__, "]\t", __VA_ARGS__)
+
+#endif // !LAB4_LOGGER_HPP
