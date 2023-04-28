485e227419df1c58dbe277e7db5475050af42b48
Dmytro Tereshchenko <buxdmo@gmail.com>
2023-04-29 01:08:47 +0300

    Lab 4: Add Timer file

---
+++ lab4/Timer.hpp
@@ -0,0 +1,32 @@
+#ifndef LAB4_TIMER_HPP
+#define LAB4_TIMER_HPP
+
+#include "Util.hpp"
+
+namespace Lab4 {
+
+	class Timer {
+	public:
+		Timer() noexcept {
+			restart();
+		}
+
+	public:
+		void restart() noexcept {
+			last = Clock::now();
+		}
+
+		template <typename Duration = std::chrono::milliseconds>
+		Duration getElapsedTime() const noexcept {
+			return std::chrono::duration_cast<Duration>(Clock::now() - last);
+		}
+
+	private:
+		TimePoint last;
+	};
+
+
+}
+
+
+#endif // !LAB4_TIMER_HPP
