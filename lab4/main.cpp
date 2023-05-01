0aa5a87a28f6e103f3c8eb34de84a0e0da52a34f
Dmytro Tereshchenko <buxdmo@gmail.com>
2023-05-02 00:26:02 +0300

    Lab 4: Update main test

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -1,38 +1,60 @@
-﻿#include <iostream>
-#include "NotificationQueue.hpp"
+﻿#include "NotificationQueue.hpp"
 #include "QueueAnalyzer.hpp"
 #include "Logger.hpp"
 #include "Timer.hpp"
+#include "Random.hpp"
 
 using namespace Lab4;
 using namespace std::chrono_literals;
-
 
 int main() {
 	NotificationQueue<int> notificationQueue{ 10 };
 	QueueAnalyzer<int> queueAnalyzer{ notificationQueue };
 
-	const auto addMessageInterval = [&notificationQueue, timer = Timer()]() {
-		LOG("START\n");
-		while (timer.getElapsedTime() <= 10000ms) {
-			notificationQueue.addMessage({ 3, 100ms, MessagePriority::Low });
-			std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 2000));
+	constexpr auto duration = 5min;
+
+	const auto addOrGetMessage = [&notificationQueue]() {
+		Timer timer;
+		while (timer.getElapsedTime() <= duration) {
+			if (Random::nextInt(0, 1)) {
+				notificationQueue.addMessage({
+					Random::nextInt(0, 1000),
+					std::chrono::milliseconds(Random::nextInt(100, 1000)),
+					static_cast<MessagePriority>(Random::nextInt(0, 3))
+				});
+			}
+			else {
+				notificationQueue.getMessage();
+			}
+			std::this_thread::sleep_for(std::chrono::milliseconds(Random::nextInt(500, 2000)));
 		}
 	};
 
-	const auto getMessageInterval = [&notificationQueue, timer = Timer()]() {
-		LOG("START\n");
-		while (timer.getElapsedTime() <= 10000ms) {
-			const auto& message = notificationQueue.getMessage();
-			std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 2000));
+	const auto getMessage = [&notificationQueue]() {
+		Timer timer;
+		while (timer.getElapsedTime() <= duration) {
+			notificationQueue.getMessage();
+			std::this_thread::sleep_for(1s);
+		}
+
+	};
+
+	const auto analyze = [&queueAnalyzer, &notificationQueue]() {
+		Timer timer;
+		auto last = Clock::now();
+		while (timer.getElapsedTime() <= duration) {
+			if (notificationQueue.isFull() || Clock::now() - last >= 1min) {
+				last = Clock::now();
+				queueAnalyzer.analyze("analysis.txt");
+			}
+			std::this_thread::sleep_for(1s);
 		}
 	};
 
-	std::jthread th1(getMessageInterval);
-	std::jthread th2(addMessageInterval);
-	std::jthread th3(getMessageInterval);
-	std::jthread th4(addMessageInterval);
-	std::jthread th5(getMessageInterval);
-	std::jthread th6(getMessageInterval);
+	std::jthread th1(addOrGetMessage);
+	std::jthread th2(addOrGetMessage);
+	std::jthread th3(addOrGetMessage);
+	std::jthread th4(getMessage);
+	std::jthread th5(analyze);
 	return 0;
 }
